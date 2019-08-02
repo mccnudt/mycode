ca93af7a99542f5b08b278782f4a29c16b6ce692
mccnudt <mccnudt@163.com>
2019-08-02 23:39:13 +0800

    测试循环写入生成excel

--- pdexcel01.py
+++ pdexcel01.py
@@ -1,6 +1,13 @@
 # 蔓藤教育pandas教程01，创建excel文件
 
 import pandas as pd
-df = pd.DataFrame({'ID': [1, 2, 3], 'name': ['Lily', 'Lucy', 'Jim']})
+l1, l2 = [], []
+for i in range(10):
+    l1.append(i)
+    l2.append(i * 10)
+
+print(l1, l2)
+df = pd.DataFrame({'ID': l1, 'name': l2})
 df.set_index('ID', inplace=True)
-df.to_excel(r'H:\CplusCode\mycode\output.xlsx')
+print(df)
+# df.to_excel(r'H:\CplusCode\mycode\output.xlsx')
